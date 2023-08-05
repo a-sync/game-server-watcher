c106c307a16affa9b95fc5ec2f1ea18827ee6912
Smith <a-sync@devs.space>
2023-08-05 16:48:12 +0200

    format new fields + styling

--- public/main.js
+++ public/main.js
@@ -33,21 +33,22 @@ $(async () => {
                 "iconlib": "fontawesome5",
                 "object_layout": "normal",
                 "template": "mustache",
+                "compact": 0,
                 "show_errors": "always",
                 "required_by_default": 0,
                 "no_additional_properties": 1,
-                "display_required_only": 1,//debug
-                "show_opt_in": 0,//debug
-                "remove_empty_properties": 0,//DEBUG2
-                "keep_oneof_values": 0,
-                "ajax": 1,//debug
+                "display_required_only": 1,
+                "show_opt_in": 0,
+                "remove_empty_properties": 0,
+                "use_default_values": 1,
+                "ajax": 1,
                 "ajaxCredentials": 0,
                 "disable_edit_json": 0,
                 "disable_collapse": 1,
-                "disable_properties": 0,//debug
+                "disable_properties": 0,
                 "disable_array_add": 0,
-                "disable_array_reorder": 1,
                 "disable_array_delete": 0,
+                "disable_array_reorder": 1,
                 "enable_array_copy": 1,
                 "array_controls_top": 0,
                 "disable_array_delete_all_rows": 1,
